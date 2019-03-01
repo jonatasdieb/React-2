e7db5a7cfa455b587410c72f1f0d9632507e2f49
jonatasdieb <jonatasdieb@gmail.com>
2019-03-01 19:08:36 +0000

    1

--- src/App.js
+++ src/App.js
@@ -3,44 +3,46 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Components/Home';
 import Departamento from './Components/Departamento/Departamento';
 import Funcionario from './Components/Funcionario/Funcionario';
-import {getUsername} from './Services/AuthService';
-import {logout} from './Services/AuthService';
+import { getUsername } from './Services/AuthService';
+import { logout } from './Services/AuthService';
 
 class App extends Component {
 
-logout(){
-    logout();
-     window.location.replace("/");
-}
+    logout() {
+        logout();
+        window.location.replace("/");
+    }
 
-  render() {
-    return (
-      <Router>
-        <div className='row'>
-        <a className='float-right' href='#' onClick={this.logout}>Logout</a>
-        </div>
-        <div class='row'>
-          <nav className="navbar navbar-expand-lg navbar-light bg-nav">
-            <div className="collapse navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <Link className="nav-link text-light" to='/'><i class="fas fa-dollar-sign text-white"></i> Despesas</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-light" to='/departamento'><i class="fas fa-briefcase text-white"></i> Departamentos</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link text-light" to='/funcionario'><i class="fas fa-users text-white"></i> Funcionários</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/departamento' component={Departamento} />
-          <Route exact path='/funcionario' component={Funcionario} />
-        </div>
-      </Router >
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <div className='row'>
+                        <a className='float-right' href='#' onClick={this.logout}>Logout</a>
+                    </div>
+                    <div class='row'>
+                        <nav className="navbar navbar-expand-lg navbar-light bg-nav">
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item active">
+                                        <Link className="nav-link text-light" to='/'><i class="fas fa-dollar-sign text-white"></i> Despesas</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link text-light" to='/departamento'><i class="fas fa-briefcase text-white"></i> Departamentos</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link className="nav-link text-light" to='/funcionario'><i class="fas fa-users text-white"></i> Funcionários</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/departamento' component={Departamento} />
+                        <Route exact path='/funcionario' component={Funcionario} />
+                    </div>
+                </div>
+            </Router >
+        );
+    }
 }
 export default App;
