dafe232754072b7a1fe8667a17392eaaa8efaabe
jonatasdieb <jonatasdieb@gmail.com>
2019-03-01 18:22:30 +0000

    2

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -7,7 +7,9 @@ export const isAuthenticated = () => api.get('/')//localStorage.getItem(TOKEN_KE
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const getUsername = () => localStorage.getItem(USERNAME);
 export const saveData = res => {
-  console.log('chegou aqui: ',res)
+  console.log('chegou no res.data.username: ',res.data.username)
+  console.log('chegou no res.data: ',res.data);
+  console.log('chegou no res.username: ',res.username);
   localStorage.setItem(TOKEN_KEY, res.token);
   localStorage.setItem(USERNAME, res.data.username)
 };
